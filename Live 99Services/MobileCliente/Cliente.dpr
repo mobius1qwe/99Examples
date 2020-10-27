1ae3bcd3facbcd6eaa8737e2083ac3d87419c77b
Mobius One <26689802+mobius1qwe@users.noreply.github.com>
2020-10-26 22:56:31 -0300

    Atualização de projeto 99 Services Aula 2

---
+++ Live 99Services/MobileCliente/Cliente.dpr
@@ -0,0 +1,15 @@
+program Cliente;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  UnitLogin in 'UnitLogin.pas' {FrmLogin},
+  UnitPrincipal in 'UnitPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFrmLogin, FrmLogin);
+  Application.Run;
+end.
