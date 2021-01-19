c1287631c431a8f31f2c65424d9ee29446f3d013
Mobius One <26689802+mobius1qwe@users.noreply.github.com>
2021-01-19 02:02:59 -0300

    Atualização de conteúdo 99 Coders:  - Exemplo Como gerar QR Code no Delphi (Android, iOS e Win) https://www.youtube.com/watch?v=AbwW-AJA_lg

---
+++ YouTube/Videos/193-GerarQrCode/QrCode.dpr
@@ -0,0 +1,15 @@
+program QrCode;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  FMXDelphiZXIngQRCode in 'FMXDelphiZXIngQRCode.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
